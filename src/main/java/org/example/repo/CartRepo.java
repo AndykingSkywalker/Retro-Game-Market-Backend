6ce54b7a5de758c3f7aca2074b27d302afedb235
AndykingSkywalker <andrewk640@gmail.com>
2026-03-14 16:21:58 +0000

    created services and controllers

--- src/main/java/org/example/repo/CartRepo.java
+++ src/main/java/org/example/repo/CartRepo.java
@@ -1,4 +1,10 @@
 package org.example.repo;
 
-public interface CartRepo {
+import org.example.domain.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+public interface CartRepo extends JpaRepository<Cart, Integer> {
+    List<Cart> findByUserId(int userId);
 }
--- src/main/java/org/example/repo/UserRepo.java
+++ src/main/java/org/example/repo/UserRepo.java
@@ -1,4 +1,9 @@
 package org.example.repo;
 
-public interface UserRepo {
+import org.example.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
 }
