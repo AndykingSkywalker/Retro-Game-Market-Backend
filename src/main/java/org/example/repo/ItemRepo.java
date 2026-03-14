1ae9055a8ffea67dad2e0ea37909430dea0c6be1
AndykingSkywalker <andrewk640@gmail.com>
2026-03-14 16:16:11 +0000

    created repo's for user and cart

--- src/main/java/org/example/repo/ItemRepo.java
+++ src/main/java/org/example/repo/ItemRepo.java
@@ -2,6 +2,8 @@ package org.example.repo;
 
 import org.example.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepo extends JpaRepository<Item, Integer> {
 }
